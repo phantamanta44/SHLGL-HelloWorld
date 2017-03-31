268392e788282abc8d7682c2c08265ab67902a7c
phantamanta44 <evandalong@gmail.com>
2017-03-31 15:34:11 -0500

    Make it work

--- src/main/java/io/github/phantamanta44/shlgl/helloworld/Main.java
+++ src/main/java/io/github/phantamanta44/shlgl/helloworld/Main.java
@@ -1,19 +1,28 @@
 package io.github.phantamanta44.shlgl.helloworld;
 
 import io.github.phantamanta44.shlgl.SHLGL;
-import io.github.phantamanta44.shlgl.engine.event.impl.GameTickEvent;
+import io.github.phantamanta44.shlgl.engine.event.IListener;
 import io.github.phantamanta44.shlgl.engine.event.impl.RenderEvent;
+import io.github.phantamanta44.shlgl.graphics.texture.TextureInfo;
+import io.github.phantamanta44.shlgl.graphics.texture.TextureManager;
 
-public class Main {
+public class Main implements IListener<RenderEvent> {
 
-
+    private static final RenderHandler renderer = new RenderHandler();
+    private static final TextureInfo tex = TextureManager.getTextureInfo("hello.png");
 
     public static void main(String[] args) {
         SHLGL.init(1280, 960, "Hello, world!");
         SHLGL.getInstance().setResolution(1280, 960);
-        SHLGL.getInstance().getEventBus().on(GameTickEvent.class, INSTANCE);
-        SHLGL.getInstance().getEventBus().on(RenderEvent.class, INSTANCE);
+        SHLGL.getInstance().getEventBus().on(RenderEvent.class, renderer);
+        SHLGL.getInstance().getGameWindow().setVisible(true);
         SHLGL.getInstance().runMainLoop(20);
+    }
+
+    @Override
+    public void onEvent(RenderEvent event) {
+        TextureManager.bind(tex);
+        event.getBuffer().drawRect(0, 0, 800, 450);
     }
 
 }
